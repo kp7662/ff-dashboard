bf9e47d217914870453ed84ec694003dbf629c8b
Kok Wei Pua <kp7662@princeton.edu>
2024-03-28 17:02:25 -0400

    test data flow for svelte stores w/ simplified model

---
+++ frontend/src/lib/StoreTest.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { selectedAffiliation } from '../lib/stores/store';
+
+    // Function to update the store
+    function updateAffiliation(newAffiliation) {
+        selectedAffiliation.set(newAffiliation);
+    }
+</script>
+
+<div>
+    <p>Current Affiliation: {$selectedAffiliation}</p>
+    <button on:click={() => updateAffiliation('CIDU')}>Set to CIDU</button>
+    <button on:click={() => updateAffiliation('RDU')}>Set to RDU</button>
+    <button on:click={() => updateAffiliation('DU')}>Set to DU</button>
+</div>
---
+++ frontend/src/routes/test/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+import DropDownMenu from "../../lib/DropDownMenu.svelte";
+import StoreTest from '../../lib/StoreTest.svelte';
+import { selectedAffiliation } from '../../lib/stores/store';
+
+let testCounter = 0;
+$: console.log('Affiliation in +page.svelte:', $selectedAffiliation, testCounter);
+
+function incrementCounter() {
+    testCounter += 1;
+    selectedAffiliation.update(n => n); // This line doesn't change the value but should trigger subscribers.
+}
+</script>
+
+<h1>Hello World</h1>
+<!-- <StoreTest /> -->
+<DropDownMenu />
+
+<p>Selected Affiliation: {$selectedAffiliation}</p>
+
+<button on:click={incrementCounter}>Increment Counter</button>
