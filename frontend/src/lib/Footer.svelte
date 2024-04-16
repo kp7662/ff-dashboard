c732ef14ed6c648253c244a029e1d74cda1151da
Kok Wei Pua <kp7662@princeton.edu>
2024-04-16 16:56:13 -0400

    updated links

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -2,11 +2,11 @@
 
 <footer class="bg-fcf8fb rounded-lg shadow dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl px-4 md:flex md:items-center md:justify-between">
-      <span class="text-lg text-gray-500 dark:text-gray-400">© 2024 <a href="https://wao.cs.princeton.edu/" class="hover:underline">FairFare™</a>. All Rights Reserved.
+      <span class="text-lg text-gray-500 dark:text-gray-400">© 2024 <a href="https://getfairfare.org/" class="hover:underline">FairFare™</a>. All Rights Reserved.
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-lg font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="https://wao.cs.princeton.edu/" class="hover:underline me-4 md:me-6">About</a>
+            <a href="https://getfairfare.org/" class="hover:underline me-4 md:me-6">About</a>
         </li>
         <li>
             <a href="." class="hover:underline me-4 md:me-6">Privacy Policy</a>
@@ -15,7 +15,7 @@
             <a href="." class="hover:underline me-4 md:me-6">Licensing</a>
         </li>
         <li>
-            <a href="https://wao.cs.princeton.edu/" class="hover:underline">Contact</a>
+            <a href="https://getfairfare.org/" class="hover:underline">Contact</a>
         </li>
       </ul>
     </div>
--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -24,7 +24,7 @@
             <a href="/deep_dive" class="block py-3 px-4 text-2xl text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Deep Dive Analysis</a>
         </li>
         <li>
-            <a href="https://wao.cs.princeton.edu/" class="block py-3 px-4 text-2xl text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+            <a href="https://getfairfare.org/" class="block py-3 px-4 text-2xl text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
     </ul>
     </div>
